3a338723c471c9984dfa193ad83c90b156c102eb
Aditya Saxena <aditya.saxena@goniyo.com>
2022-11-19 18:04:33 +0530

    basic ci/cd setup

---
+++ src/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports.lambda_handler = async (event) => {
+
+  console.log("Welcome to serverless-dynamoosejs-get lambda function!")
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
