55ba03ad734a2e4003c31e161cc3c0dac3732261
Aditya Saxena <aditya.saxena@goniyo.com>
2022-11-22 02:49:24 +0530

    update joinedDate ts

--- dynamo-populate/index.js
+++ dynamo-populate/index.js
@@ -14,7 +14,7 @@ const userSchema = new dynamoose.Schema({
         "enum": ['Female', 'Genderfluid', 'Male', 'Polygender', 'Bigender', 'Agender', 'Non-binary', 'Genderqueer']
     },
     "ipAddress": String,
-    "dateJoined": Number,
+    "dateJoined": String,
 }, {
     "saveUnknown": false,
     "timestamps": true
@@ -26,14 +26,29 @@ const User = dynamoose.model("staging-usersTable", userSchema, {
     "waitForActive": false
 });
 
-(async () => {
-    data.forEach(async userData => {
-        try {
-            const user = await User.create(userData); // If a user with `id=1` already exists in the table, an error will be thrown.
-            console.log(user);
-        } catch (error) {
-            console.error(error);
-        }
-    });
-})()
+// (async () => {
+//     data.forEach(async userData => {
+//         try {
+//             userData.joinedDate = new Date(userData.joinedDate).toLocaleDateString()
+//             const user = await User.create(userData); // If a user with `id=1` already exists in the table, an error will be thrown.
+//             console.log(user);
+//         } catch (error) {
+//             console.error(error);
+//         }
+//     });
+// })()
 
+const updatedData = data.map(userData => {
+    return { ...userData, joinedDate: new Date(new Date(userData.joinedDate).toLocaleDateString()).getTime() }
+})
+
+    // Update table
+    (async () => {
+        await User.batchPut(updatedData, (error) => {
+            if (error) {
+                console.error(error);
+            } else {
+                console.log(result);
+            }
+        });
+    })()
